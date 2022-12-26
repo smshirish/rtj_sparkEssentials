484048b93383240b3fe71ca160d5634a1c812dc3
shiri <shirishsakhare@gmail.com>
2022-12-26 18:50:14 +0100

    Start of chapter 2-Data frames basics

---
+++ readme-shirish.txt.txt
@@ -0,0 +1,7 @@
+This project contains the spark esentials course original material - as downloaded from internet.
+
+Set up Notes :
+Docker -> Was already available.
+Winutils -> Need to install.  Added hadoop home as D:\programs\winutils-master\hadoop-3.2.2\bin
+
+go to directory spark-cluster and run build-images.sh
--- src/main/scala/part2dataframes/DataFramesBasics.scala
+++ src/main/scala/part2dataframes/DataFramesBasics.scala
@@ -4,8 +4,13 @@ import org.apache.spark.sql.SparkSession
 
 object DataFramesBasics extends App{
 
+  //create spark session
   val spark = SparkSession.builder()
             .appName("Dataframe Basics")
     .config("spark.master","local")
             .getOrCreate()
+
+  //read data frame
+
+  spark.read.format("json").load("")
 }
