6322b57cc5a57781c1c8c829376657d56cf0be36
shiri <shirishsakhare@gmail.com>
2022-12-27 10:12:33 +0100

    chapter 2-Data frames basics-write operation

--- src/main/scala/part2dataframes/DataSources.scala
+++ src/main/scala/part2dataframes/DataSources.scala
@@ -1,7 +1,7 @@
 package part2dataframes
 
 import org.apache.spark.sql.{SaveMode, SparkSession}
-import org.apache.spark.sql.types.{DoubleType, LongType, StringType, StructField, StructType}
+import org.apache.spark.sql.types.{DateType, DoubleType, LongType, StringType, StructField, StructType}
 
 object DataSources extends App{
 
@@ -18,7 +18,7 @@ object DataSources extends App{
     StructField("Horsepower", LongType),
     StructField("Weight_in_lbs", LongType),
     StructField("Acceleration", DoubleType),
-    StructField("Year", StringType),
+    StructField("Year", DateType),
     StructField("Origin", StringType)
   ))
 
@@ -67,5 +67,100 @@ object DataSources extends App{
   )
 
   spark.createDataFrame(cars).write.format("json").mode(SaveMode.Overwrite).save("src/main/resources/data/cars_duplicate.json")
+
+  /**
+    * Spark read/write options for various formats
+    */
+  spark.read
+    .schema(carsSchema)
+    .option("dateFormat","YYYY-MM-dd") // couple it with schema , without schema it cant know which is date type.If spark fails to parse date,it puts null
+    .option("allowSingleQuotes","true")
+    .option("compression","true") // bzip2, gzip, lz4, snappy, deflate
+    .format("json")
+    .load("src/main/resources/data/cars.json")  //the format and load calls can be combined to call .json("path method")
+
+  //csv options  symbol,date,price
+  val stockSchema = StructType(Array(
+    StructField("symbol",StringType)
+    ,StructField("date",DateType)
+    ,StructField("price",DoubleType)
+  ))
+
+  spark.read
+    .schema(stockSchema)
+    .option("dateFormat","MMM dd YYYY")
+    .option("header","true")
+    .option("sep",",")
+    .option("nullValue","")
+    //.format("csv")
+    .csv("src/main/resources/data/stocks.csv")  // this replaces .format("csv").load(path)
+
+  //parquet format
+  carsDF.write
+    .mode(SaveMode.Overwrite)
+    .parquet("src/main/resources/data/cars.parquet")  //parquet is default format, no need to provide format("parquet")
+
+  //text files
+  spark.read.text("src/main/resources/data/sampleTextFile.txt")
+
+  //reading from remote DB
+  val employeesDF = spark.read
+    .format("jdbc")
+    .option("driver","org.postgresql.Driver")
+    .option("url","jdbc:postgresql://localhost:5432/rtjvm")
+    .option("user","docker")
+    .option("password","docker")
+    .option("dbtable","public.employees")
+    .load()
+
+  employeesDF.show()
+
+  /**
+    * Exercise :  Read movies dataframe and then write it as
+    * -tab seperated values file
+    * -snappy parquet
+    * - table public.movies in the postgres DB
+    */
+
+   val moviesDF = spark.read
+     .option("inferSchema","true")
+     .json("src/main/resources/data/movies.json")
+
+  println("!!!!!")
+  moviesDF.show
+
+
+   moviesDF.write
+     .mode(SaveMode.Overwrite)
+     .option("header","true")
+     .option("sep","\t")
+     .csv("src/main/resources/data/movies.csv")
+
+  moviesDF.write
+    .mode(SaveMode.Overwrite)
+   // .option("compression","snappy") not needed as its default
+    .parquet("src/main/resources/data/movies.parquet")
+
+  moviesDF.write
+    .format("jdbc")
+    .option("driver","org.postgresql.Driver")
+    .option("url","jdbc:postgresql://localhost:5432/rtjvm")
+    .option("user","docker")
+    .option("password","docker")
+    .option("dbtable","public.movies")
+    .mode(SaveMode.Overwrite)
+    .save()
+
+  val moviesDFFromDB = spark.read
+    .format("jdbc")
+    .option("driver","org.postgresql.Driver")
+    .option("url","jdbc:postgresql://localhost:5432/rtjvm")
+    .option("user","docker")
+    .option("password","docker")
+    .option("dbtable","public.movies")
+    .load()
+  println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+  moviesDFFromDB.show()
+
 
 }
