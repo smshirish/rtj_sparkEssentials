38cf0d9bb319581a9eafd6faa1f9a79b01d9c594
shiri <shirishsakhare@gmail.com>
2023-01-04 12:40:10 +0100

    chapter 3- Datasets

---
+++ src/main/scala/part3typesdatasets/Datasets.scala
@@ -0,0 +1,89 @@
+package part3typesdatasets
+
+import org.apache.spark.sql.{Dataset, Encoders, SparkSession}
+import org.apache.spark.sql.functions._
+
+import java.sql.Date
+
+
+object Datasets extends App{
+
+  val spark = SparkSession.builder()
+    .config("spark.master","local")
+    .appName("Datasets")
+    .getOrCreate()
+
+
+  val numbersDF = spark.read
+    .option("header","true")
+    .option("inferSchema","true")
+    .format("csv")
+    .load("src/main/resources/data/numbers.csv")
+
+  ///numbersDF.printSchema()
+
+  //create types dataset from DF.
+  implicit val intEncoder = Encoders.scalaInt
+  val numbersDS :Dataset[Int] = numbersDF.as[Int]
+
+  numbersDS.filter(_ < 100)  //normal scala operations can be applied
+   /// .show(300,false)
+
+
+  //{"Name":"amc ambassador dpl", "Miles_per_Gallon":15, "Cylinders":8, "Displacement":390,
+  // "Horsepower":190, "Weight_in_lbs":3850, "Acceleration":8.5, "Year":"1970-01-01", "Origin":"USA"}
+  case class Car(
+                  Name:String ,
+                  Miles_per_Gallon:Option[Double],
+                  Cylinders:Long,
+                  Displacement:Double,
+                  Horsepower: Option[Long],
+                  Weight_in_lbs:Long,
+                  Acceleration:Double,
+                  Year:String,
+                  Origin:String
+                )
+
+  private def readDF (fileName:String) = {
+    spark.read
+      .option("inferSchema", "true")
+      .json(s"src/main/resources/data/$fileName")
+  }
+  val carsDF = readDF("cars.json")
+
+  /// carsDF.show()
+  //below imports not needed is you import spark.implicits...the imports are to illustrate the encoder mechanism
+  //add encoder for Car
+  //implicit val carEncoder = Encoders.product[Car] //all case classes extend Product type
+  //implicit val stringEncoder = Encoders.STRING
+
+
+  import spark.implicits._
+  val carsDS :Dataset[Car] = carsDF.as[Car]
+  carsDS.map( car => car.Name.toUpperCase())
+    .show()
+
+  /**
+    * exercise:
+    * How many cars we have
+    * How many poserful cars we have (HP > 100)
+    * Average horsepower of the dataset
+    *
+    */
+
+  //how many cars we have
+  private val carsCount: Long = carsDS.count()
+  println(carsCount)
+
+  //powerful cars
+  carsDS.filter( car =>  car.Horsepower.getOrElse(0L) > 140L)
+    .show()
+
+  val avgHP =  carsDS.map(_.Horsepower.getOrElse(0L)).reduce(_ + _ )/ carsCount
+  println(avgHP)
+
+  //DS also has access to all DF functions
+  carsDS.select(avg(col("Horsepower")))
+    .show()
+
+}
--- src/main/scala/part3typesdatasets/ManagingNulls.scala
+++ src/main/scala/part3typesdatasets/ManagingNulls.scala
@@ -30,9 +30,32 @@ object ManagingNulls extends App{
 
   //nulls when ordering
   moviesDF.select("*").orderBy(col("Rotten_Tomatoes_Rating").desc_nulls_first)
-   // .show()
+   /// .show()
 
   //dropping nulls
-  moviesDF.select("*").na.drop()
-    .show()
+  moviesDF.select(col("Title"),col("Rotten_Tomatoes_Rating")).na.drop()
+    ///.show()
+
+  //replace nulls (fill all columns with same replacement)
+  moviesDF.na.fill(0, List("Rotten_Tomatoes_Rating","IMDB_Rating"))
+  ///  .show()
+
+  //replace nulls , diff value for each column
+  moviesDF.na.fill(Map(
+    "Rotten_Tomatoes_Rating" -> 999,
+    "IMDB_Rating" -> 11111,
+    "Director" -> "Unknown")
+  )
+  ///  .show(300)
+
+  //nulls , complex expressions
+  moviesDF.selectExpr("Title",
+    "Rotten_Tomatoes_Rating",
+    "IMDB_Rating",
+    "ifnull(Rotten_Tomatoes_Rating, IMDB_Rating * 10) as ifnull",  //same as coalesce above -> show Rotten_Tomatoes_Rating if not null , else imdb_Rating * 10
+    "nvl(Rotten_Tomatoes_Rating, IMDB_Rating * 10) as nvl ", //same as above
+    "nullif(Rotten_Tomatoes_Rating, IMDB_Rating * 10) as nullif " , // returns null if the 2 values are equal  , if two values are not equal, return first value
+    "nvl2(Rotten_Tomatoes_Rating, IMDB_Rating * 10,0) as nvl2" //  if (first != null) second else third --->  if first argument not null, return second argument , otheriswe the third argument
+  )
+    .show(200)
 }
